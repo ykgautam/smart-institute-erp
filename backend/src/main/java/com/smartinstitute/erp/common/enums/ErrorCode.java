18291579fd95e8ac572bd0d4162eee6bdf816c03
Yashkumar Gautam <yk.gautam97@gmail.com>
2026-06-28 00:21:23 +0530

    project-setup(user): exception changes

---
+++ backend/src/main/java/com/smartinstitute/erp/common/enums/ErrorCode.java
@@ -0,0 +1,13 @@
+package com.smartinstitute.erp.common.enums;
+
+public enum ErrorCode {
+
+    RESOURCE_NOT_FOUND,
+    DUPLICATE_RESOURCE,
+    BUSINESS_ERROR,
+    VALIDATION_ERROR,
+    UNAUTHORIZED,
+    FORBIDDEN,
+    INTERNAL_SERVER_ERROR
+
+}
---
+++ backend/src/main/java/com/smartinstitute/erp/common/exception/DuplicateResourceException.java
@@ -0,0 +1,9 @@
+package com.smartinstitute.erp.common.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+
+}
